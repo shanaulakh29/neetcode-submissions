3afda666f6cf4758e488e8eb71ec7e21434bab16
Gurshan  Aulakh <166259577+shanaulakh29@users.noreply.github.com>
2026-08-08 09:48:10 -0700

    Add: last-stone-weight - submission-4

---
+++ Data Structures & Algorithms/last-stone-weight/submission-4.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int>pq;
+        for(int i=0;i<stones.size();i++){
+            pq.push(stones[i]);
+        }
+        while(pq.size()>1){
+            int x = pq.top();
+            pq.pop();
+            int y = pq.top();
+            pq.pop();
+            if(x<y){
+                pq.push(y-x);
+            }else if(x>y){
+                pq.push(x-y);
+            }
+        }
+        if(pq.size()==1){
+            return pq.top();
+        }else{
+            return 0;
+        }
+    }
+};
