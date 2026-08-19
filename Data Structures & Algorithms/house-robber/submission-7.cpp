bfeba032f0cc3aa70c905170d2469194c1e9d141
Gurshan  Aulakh <166259577+shanaulakh29@users.noreply.github.com>
2026-08-18 21:46:01 -0700

    Add: house-robber - submission-7

---
+++ Data Structures & Algorithms/house-robber/submission-7.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if(nums.size()==1){
+            return nums[0];
+        }
+        int size=nums.size();
+        vector<int>dp(size);
+        dp[0]=nums[0];
+        dp[1]=max(nums[1], nums[0]);
+        for(int i=2;i<nums.size();i++){
+            dp[i]=max(nums[i]+dp[i-2], dp[i-1]);
+        }
+        return dp[nums.size()-1];
+    }
+};
